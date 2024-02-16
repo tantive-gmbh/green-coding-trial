70e6caad41aa3fcb11efb7a3d86ae0eccd4663e1
Udo Nesshoever <dev@nesshoever.de>
2024-02-16 09:37:57 +0100

    updated console app output

--- HighLoad.Console/Program.cs
+++ HighLoad.Console/Program.cs
@@ -5,11 +5,15 @@ internal class Program
     public static void Main(string[] args)
     {
         var helper = Helper.Instance;
-        helper.Initialize(1);
+        Console.WriteLine($"Helper instance: {helper.IdentityTag}");
+        helper.Initialize(3, 2);
         // helper.Initialize(3,1);
         foreach (var entry in helper.Dict)
         {
-            Console.WriteLine($"{entry.Key} = {entry.Value}");
+            Console.WriteLine($"{entry.Key} = {entry.Value.MaxLen(80)}");
         }
+
+        Console.WriteLine("=== Press ENTER to close the console ===");
+        Console.ReadLine();
     }
 }
